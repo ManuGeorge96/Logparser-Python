ab48b85b917fbb2cc36cb7283f549cf864d95a96
Manu George <92159747+ManuGeorge96@users.noreply.github.com>
2022-04-03 15:05:48 +0530

    initial

---
+++ log.py
@@ -0,0 +1,26 @@
+import parser
+file = open('access.log','r')
+frequency = {}
+for line in file:
+ part = parser.parser(line)
+ date = part['time'][0:11]
+ time = part['time'][12:14]
+ status = part['status']
+ if date not in frequency:
+   frequency[date] = {time:{status:1}}
+ else:
+   if time not in frequency[date]:
+     frequency[date][time] = {status:1}
+   else:
+     if status not in frequency[date][time]:
+       frequency[date][time][status] = 1
+     else:
+       frequency[date][time][status] = frequency[date][time][status]+ 1
+file.close()
+#print(frequency)
+for date in frequency:
+ print("\nDate::", date)
+ for hour in frequency[date]:
+   print(" Hour::", hour)
+   for status in frequency[date][hour]:
+     print(" Status::", status , "Count::", frequency[date][hour][status])
